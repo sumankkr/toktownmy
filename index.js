2d7070a2240c6c77af742a1b7195f35f116e43db
Biswabrata Sarkar <biswabrata77@gmail.com>
2020-06-24 21:58:35 +0530

    dne

--- index.js
+++ index.js
@@ -76,5 +76,5 @@ Server.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+    console.log('running on port 3000');
 });
