6c95442abf48ef6a0ca7d66417d30f7b1465556f
Biswabrata Sarkar <biswabrata77@gmail.com>
2020-06-27 16:22:00 +0530

    Update speedtest_worker.js

--- public/speedtest_worker.js
+++ public/speedtest_worker.js
@@ -518,7 +518,7 @@ function ulTest(done) {
 						
 						xhr[i].open("POST", settings.url_ul + url_sep(settings.url_ul) + (settings.mpot ? "cors=true&" : "") + "r=" + Math.random(), true); // random string to prevent caching
 						try {
-							xhr[i].setRequestHeader("Content-Encoding", "compress");// disable compression (some browsers may refuse it, but data is incompressible anyway)
+							xhr[i].setRequestHeader("Content-Encoding", "identity,compress,deflate,br,gzip");// disable compression (some browsers may refuse it, but data is incompressible anyway)
 						} catch (e) {}
 						//No Content-Type header in MPOT branch because it triggers bugs in some browsers
 					
